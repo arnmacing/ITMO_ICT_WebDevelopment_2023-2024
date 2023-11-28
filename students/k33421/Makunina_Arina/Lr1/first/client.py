7ffc8bc9c74bcb93fbd489b92c8d26582e27b833
arnmacing <93253694+arnmacing@users.noreply.github.com>
2023-11-28 15:36:41 +0300

    Deployed 35c5fac with MkDocs version: 1.5.3

--- students/k33421/Makunina_Arina/Lr1/first/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('localhost', 5433)
-message = "Hello, server"
-client_socket.sendto(message.encode(), server_address)
-data, _ = client_socket.recvfrom(1024)
-print(f"Получено сообщение от сервера: {data.decode()}")
-client_socket.close()
